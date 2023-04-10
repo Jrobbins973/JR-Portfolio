57cd65d29c90ce0ef004ded5dd31d6e9e0002e21
Joe Robbins <joe.robbins.cpt@gmail.com>
2023-04-10 13:37:05 -0400

    some more touches to the hero 3d model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,10 +6,14 @@ import CanvasLoader from '../Loader'
 
 
 
-const Computers = () => {
+const Computers = ({isMobile}) => {
 
-  const computer = useGLTF('/public/desktop_pc/scene.gltf')
+  
 
+
+
+
+  const computer = useGLTF('./desktop_pc/scene.gltf')
 
   return (
     <mesh>
@@ -17,14 +21,17 @@ const Computers = () => {
       intensity={0.15} 
       groundColor='black'/>
 
-      <pointLight intensity={0.5}/>
+      <pointLight intensity={1}/>
       <spotLight 
       position={[-20, 50, 10]}
+      angle={0.12}
+      penumbra={1}
+      intensity={1}
       />
       <primitive 
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.4 : 0.6}
+        position={isMobile ? [0, -1, -1.2]:[0, -2.25, -0.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
 
@@ -33,6 +40,26 @@ const Computers = () => {
 }
 
 const ComputersCanvas = () => {
+
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)')
+
+    setIsMobile(mediaQuery.matches)
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches)
+    }
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange)
+
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }  
+  }, [])
+
   return (
     <Canvas
     frameloop='demand'
@@ -47,10 +74,11 @@ const ComputersCanvas = () => {
         maxPolarAngle = {Math.PI / 2}
         minPolarAngle = {Math.PI / 2}
         />
-      <Computers />
+      <Computers isMobile = {isMobile}/>
       </Suspense>
     <Preload all />
     </Canvas>
+
   )
 }
 
